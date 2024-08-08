98715680b6b935878cb1fd462e803cbc27307a74
insanogoku <aykelsisas@gmail.com>
2024-08-08 18:50:23 -0500

    hice una variable para el numero mayor

--- home.py
+++ home.py
@@ -1,2 +1,26 @@
 print ("Hola mundo")
 print ("Si eres capaz de leer esto es por que aprendiste a actualizar el repositorio, FELICIDADES")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = 3
+b = 2
+
+if a > b: 
+    print (a)
