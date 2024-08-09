4bed190f9f608bb262df0d9b3b392428dde6cab4
josedanielgil09 <josedanielgil09@gmail.com>
2024-08-08 19:14:40 -0500

    agregue un algoritmo que define si una letra esvocal o no (lineas del 30 al 35)

--- home.py
+++ home.py
@@ -1,2 +1,35 @@
 print ("Hola mundo")
 print ("Si eres capaz de leer esto es por que aprendiste a actualizar el repositorio, FELICIDADES")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vocales=["a","e","i","o","u"]
+letra=input("ingrese una letra \n")
+if letra in vocales:
+    print('tu letra es vocal')
+else:
+    print("tu letra no es vocal")
