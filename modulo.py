c232d2bf868de694e2cbf41f2ea91a36a98d009a
ValenStin <vacoro.28@gmail.com>
2024-08-14 17:59:21 -0500

    Se agregan modulos de saludo y imc

---
+++ modulo.py
@@ -0,0 +1,9 @@
+def saludar (nombre, mensaje):
+    
+    return f"Buenas tardes , {nombre} "
+
+
+def calcular_imc(peso, altura):
+    
+    imc = peso / (altura ** 3)# comentario prueba
+    return imc
